8c4bd0200c429f9da427722ef6dbdff7c8829b3b
Khaled Muamar <khaledm1990.km@gmail.com>
2026-01-25 23:36:39 +0100

    Fix cors issue and return a static json response

---
+++ app/controllers/api/v1/playlists_controller.rb
@@ -0,0 +1,10 @@
+module Api
+  module V1
+    class PlaylistsController < ApiController
+      RESPONSE =
+      def index
+        render json: API_RESPONSE
+      end
+    end
+  end
+end
---
+++ config/initializers/CONSTANTS.rb
@@ -0,0 +1,94 @@
+API_RESPONSE = {
+  "total_count_text": "125 Songs",
+  "total_duration_text": "5.5 hours",
+  "playlists": [
+    {
+      "id": "1",
+      "name": "Bass House",
+      "cover_url": "/images/playlists/bass-house.jpg",
+      "tracks": [
+        {
+          "id": "101",
+          "title": "Take the Crown",
+          "artist_name": "Steven Cooper",
+          "cover_url": "/images/artists/steven-cooper.jpg",
+          "duration": "02:17"
+        },
+        {
+          "id": "102",
+          "title": "Don’t Stop",
+          "artist_name": "Juicy WRLD",
+          "cover_url": "/images/artists/juicy-wrld.jpg",
+          "duration": "02:17"
+        },
+        {
+          "id": "103",
+          "title": "Feel the Vibe",
+          "artist_name": "DJ Snake",
+          "cover_url": "/images/artists/dj-snake.jpg",
+          "duration": "02:17"
+        },
+        {
+          "id": "104",
+          "title": "Lose Control",
+          "artist_name": "Missy Elliott",
+          "cover_url": "/images/artists/missy-elliott.jpg",
+          "duration": "02:17"
+        }
+      ]
+    },
+    {
+      "id": "2",
+      "name": "Dance Pop",
+      "cover_url": "/images/playlists/dance-pop.jpg",
+      "tracks": [
+        {
+          "id": "201",
+          "title": "Take the Crown",
+          "artist_name": "Steven Cooper",
+          "cover_url": "/images/artists/steven-cooper.jpg",
+          "duration": "02:17"
+        },
+        {
+          "id": "202",
+          "title": "Don’t Stop",
+          "artist_name": "Juicy WRLD",
+          "cover_url": "/images/artists/juicy-wrld.jpg",
+          "duration": "02:17"
+        },
+        {
+          "id": "203",
+          "title": "Feel the Vibe",
+          "artist_name": "DJ Snake",
+          "cover_url": "/images/artists/dj-snake.jpg",
+          "duration": "02:17"
+        },
+        {
+          "id": "204",
+          "title": "Lose Control",
+          "artist_name": "Missy Elliott",
+          "cover_url": "/images/artists/missy-elliott.jpg",
+          "duration": "02:17"
+        }
+      ]
+    },
+    {
+      "id": "3",
+      "name": "Deep House",
+      "cover_url": "/images/playlists/deep-house.jpg",
+      "tracks": []
+    },
+    {
+      "id": "4",
+      "name": "Drum & Bass Bangers",
+      "cover_url": "/images/playlists/dnb.jpg",
+      "tracks": []
+    },
+    {
+      "id": "5",
+      "name": "Dubsteps",
+      "cover_url": "/images/playlists/dubsteps.jpg",
+      "tracks": []
+    }
+  ]
+}
