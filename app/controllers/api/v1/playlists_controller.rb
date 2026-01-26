40aced99e3d9ad57530250f507b1aaf63ef3fc56
Khaled Muamar <khaledm1990.km@gmail.com>
2026-01-26 14:39:59 +0100

    change the playlists query to run run an N+1 query on the nested resources

--- app/controllers/api/v1/playlists_controller.rb
+++ app/controllers/api/v1/playlists_controller.rb
@@ -1,5 +1,15 @@
 class Api::V1::PlaylistsController < ApiController
+  before_action :set_playlists, only: [ :index ]
   def index
-    render json: Api::V1::Playlists::ResponsePresenter.new(playlists: Playlist.all).response
+    render json: Api::V1::Playlists::ResponsePresenter.new(playlists: playlists).response
+  end
+
+
+
+  private
+  attr_reader :playlists
+
+  def set_playlists
+    @playlists = Playlist.includes(tracks: :artist).select(:id, :name)
   end
 end
